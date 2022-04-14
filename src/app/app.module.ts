5983546485adbc7b94a7b96993baab977f25e1e2
ahmed morsi <ahmed.morsi10@hotmail.com>
2022-04-15 01:34:37 +0200

    Display Product And Cards in shopping Cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/shared/header/header.component';
+import { FooterComponent } from './Components/shared/footer/footer.component';
+import { NavComponent } from './Components/shared/nav/nav.component';
+import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
+import { FiltersComponent } from './Components/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './Components/shopping-cart/product-list/product-list.component';
+import { CartComponent } from './Components/shopping-cart/cart/cart.component';
+import { CartItemComponent } from './Components/shopping-cart/cart/cart-item/cart-item.component';
+import { ProductItemComponent } from './Components/shopping-cart/product-list/product-item/product-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    ShoppingCartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    CartComponent,
+    CartItemComponent,
+    ProductItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
