c353ba24ea5e5d81af9f543793413d2ae07e9aad
Sara2082998 <sarakhalaf211@gmail.com>
2022-04-16 23:50:53 +0200

    slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,8 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HeaderComponent } from './Components/shared/header/header.component';
-import { FooterComponent } from './Components/shared/footer/footer.component';
-import { NavComponent } from './Components/shared/nav/nav.component';
+
+
 import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
 import { FiltersComponent } from './Components/shopping-cart/filters/filters.component';
 import { ProductListComponent } from './Components/shopping-cart/product-list/product-list.component';
@@ -14,26 +12,33 @@ import { CartComponent } from './Components/shopping-cart/cart/cart.component';
 import { CartItemComponent } from './Components/shopping-cart/cart/cart-item/cart-item.component';
 import { ProductItemComponent } from './Components/shopping-cart/product-list/product-item/product-item.component';
 import { NgModule } from '@angular/core';
+import { NavComponent } from './Components/shared/nav/nav.component';
+import { HeaderComponent } from './Components/shared/header/header.component';
+import { SliderComponent } from './Components/shared/slider/slider.component';
+import { FooterComponent } from './Components/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavComponent,
+   HeaderComponent,
+   FooterComponent,
+   NavComponent,
     ShoppingCartComponent,
     FiltersComponent,
     ProductListComponent,
     CartComponent,
     CartItemComponent,
-    ProductItemComponent
+    ProductItemComponent,
+    SliderComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
+    
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
