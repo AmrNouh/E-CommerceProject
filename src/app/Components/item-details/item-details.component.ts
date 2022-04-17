190ee5fa5340c155396f6bdcc98ccc90ea5c8d4f
Amr M. Nouh <AMR.MAMDOUH1371@ci.menofia.edu.eg>
2022-04-17 23:45:31 +0200

    Routing + ProductDetails

--- src/app/Components/item-details/item-details.component.ts
+++ src/app/Components/item-details/item-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-item-details',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemDetailsComponent implements OnInit {
 
-  constructor() { }
+  productId: number;
+  product: any;
+  constructor(private activeRoute: ActivatedRoute, private myService: ProductsService) {
+    this.productId = this.activeRoute.snapshot.params["id"];
+  }
 
   ngOnInit(): void {
+    this.myService.getProductById(this.productId).subscribe(
+      (data) => {
+        this.product = data;
+      }
+    )
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ItemDetailsComponent } from './Components/item-details/item-details.component';
+import { ShoppingCartComponent } from './Components/shopping-cart/shopping-cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "Home", pathMatch: "full" },
+  { path: "Home", component: ShoppingCartComponent },
+  { path: "Details/:id", component: ItemDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
