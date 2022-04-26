72d48b607c12fe080675d9b94328eee89b2b6c89
Amr M. Nouh <AMR.MAMDOUH1371@ci.menofia.edu.eg>
2022-04-26 04:24:23 +0200

    Merge branch 'master' of https://github.com/AmrNouh/E-CommerceProject

--- src/app/Components/shopping-cart/add-product/add-product.component.ts
+++ src/app/Components/shopping-cart/add-product/add-product.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductsService } from 'src/app/services/products.service';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
- 
-  
+  selectedFile:File = null;
+  imgPath = '../../../assets/ProductImages/';
+  product: Product;
+    constructor(private myService:ProductsService , private http:HttpClient) { }
+    
+    onFileChanged(event) {
+    
+      const file = event.target.files[0].name ;
+      this.product.imageUrl = this.imgPath + file;
+      console.log(file);
+    }
+    
 
-    constructor(private myService:ProductsService) { }
-    
-    
     ngOnInit(): void {
     }
-  
     AddNew(name:string,price: string| number,imageUrl:string,rating:string| number,discount: string|number,category:string,color:string,quantity:string|number,size:string){
       let product=new Product();
     product.name = name;
@@ -24,7 +30,7 @@ export class AddProductComponent implements OnInit {
     product.imageUrl = imageUrl;
     product.rating = +rating;
     product.discount =+ discount;
-   product.category = category;
+    product.category = category;
     product.color = color;
     product.quantity =+ quantity;
     product.size=size;
